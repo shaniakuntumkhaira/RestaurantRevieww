d28342eb4b2629268543864dbe0b175b2e380eef
shaniakuntumkhaira <12250123545@students.uin-suska.ac.id>
2024-05-02 22:17:15 +0700

    .

--- app/src/main/java/com/shania/tugas7/data/retrofit/ApiService.java
+++ app/src/main/java/com/shania/tugas7/data/retrofit/ApiService.java
@@ -1,15 +1,17 @@
-package com.shania.tugas7.data.retrofit
+package com.shania.tugas7.data.retrofit;
 
+import com.shania.tugas7.data.response.RestaurantResponse;
+import com.shania.tugas7.data.response.PostReviewResponse;
 
+import retrofit2.Call;
 import retrofit2.http.*;
-
 
 public interface ApiService {
     @GET("detail/{id}")
     Call<RestaurantResponse> getRestaurant(@Path("id") String id);
 
     @FormUrlEncoded
-    @Headers({"Authorization: token 12345"})
+    @Headers("Authorization: token 12345")
     @POST("review")
     Call<PostReviewResponse> postReview(@Field("id")String id,@Field("name")String name,@Field("review")String review);
 }
